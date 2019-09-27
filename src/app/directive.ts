2968399169c7537c892871133c4b5f2fd91decd0
Mihyar Al Masalma <eng.mihyear@gmail.com>
2019-09-27 13:37:07 -0300

    remove directive

--- src/app/directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, ViewContainerRef } from "@angular/core";
-
-@Directive({
-    selector: '[processContainer]'
-})
-
-export class directive {
-    constructor(public viewContainerRef : ViewContainerRef){}
-}
