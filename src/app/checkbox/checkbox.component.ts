a0ee0134b06a01ca205bddf7199394a37a27e3b3
Mihyar Al Masalma <eng.mihyear@gmail.com>
2019-11-12 12:55:50 -0400

    Dynamic Disable and required + fix id's

--- src/app/checkbox/checkbox.component.ts
+++ src/app/checkbox/checkbox.component.ts
@@ -9,13 +9,24 @@ import { ProcessComponent } from '../process';
 export class CHECKBOXComponent implements ProcessComponent {
 
 	label: string;
+	required = false;
+	disabled = false;
+	id : string;
 	@Input() data;
 
 	constructor() { }
 
 	ngOnInit() {
 		this.label = this.data.POS_LABEL;
-		if (this.data.POS_REQUIRED == '1') this.label = '*' + this.label;
+		let name = this.data.POS_DICT_NAME;
+		this.id = name[0] == "#"? name.slice(1,name.length) : name;
+		if (this.data.POS_REQUIRED == '1'){
+			this.label = '*' + this.label;
+			this.required = true;
+		}
+		if (this.data.POS_CONTROL_DISABLED == '1') {
+			this.disabled = true;
+		}
 	}
 
 }
